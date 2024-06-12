01516dffe9e10f7a1963cb4c6d63af8ed8e5a334
Sebastian Landwehr <info@sebastianlandwehr.com>
2024-06-12 18:20:52 +0000

    feat: init

---
+++ src/index.js
@@ -0,0 +1,50 @@
+import { transform } from '@babel/core'
+import { endent } from '@dword-design/functions'
+import { parseVueRequest } from '@vitejs/plugin-vue'
+import { generateCodeFrame } from '@vue/compiler-dom'
+import { parse } from 'vue/compiler-sfc'
+import vueSfcDescriptorToString from 'vue-sfc-descriptor-to-string'
+
+export default () => ({
+  enforce: 'pre',
+  transform: async (code, id) => {
+    const query = parseVueRequest(id)
+    if (
+      query.filename.endsWith('.vue') &&
+      query.query.type !== 'style' &&
+      !query.filename.split('/').includes('node_modules')
+    ) {
+      const sfc = parse(code)
+      for (const section of ['scriptSetup', 'script']) {
+        if (
+          sfc.descriptor[section] &&
+          sfc.descriptor[section].lang === undefined
+        ) {
+          try {
+            sfc.descriptor[section].content = (
+              await transform(sfc.descriptor[section].content, {
+                filename: query.filename,
+              })
+            ).code
+          } catch (error) {
+            error.message = endent`
+              [vue/compiler-sfc] ${error.message.split('\n')[0]}
+
+              ${query.filename}
+              ${generateCodeFrame(
+                sfc.descriptor.source,
+                error.pos + sfc.descriptor[section].loc.start.offset,
+                error.pos + sfc.descriptor[section].loc.start.offset + 1,
+              )}
+            `
+            throw error
+          }
+        }
+      }
+
+      return vueSfcDescriptorToString(sfc.descriptor)
+    }
+
+    return code
+  },
+})
---
+++ src/index.spec.js
@@ -0,0 +1,43 @@
+import { endent } from '@dword-design/functions'
+import tester from '@dword-design/tester'
+import testerPluginTmpDir from '@dword-design/tester-plugin-tmp-dir'
+import packageName from 'depcheck-package-name'
+import { execaCommand } from 'execa'
+import outputFiles from 'output-files'
+
+export default tester(
+  {
+    works: async () => {
+      await outputFiles({
+        'babel.config.json': JSON.stringify({
+          plugins: [
+            [
+              packageName`@babel/plugin-proposal-pipeline-operator`,
+              { proposal: 'fsharp' },
+            ],
+          ],
+        }),
+        'nuxt.config.js': endent`
+          import self from '../src/index.js'
+
+          export default {
+            vite: {
+              plugins: [self()],
+            },
+          }
+        `,
+        'pages/index.vue': endent`
+          <template>
+            {{ foo }}
+          </template>
+
+          <script setup>
+          const foo = x |> x => x * 2
+          </script>
+        `,
+      })
+      await execaCommand('nuxt build')
+    },
+  },
+  [testerPluginTmpDir()],
+)
